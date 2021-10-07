b8d460d353af6936806ba21f9cc655ae96b29018
absolutelynoot <strongsilvers.ahboi@gmail.com>
2021-10-07 21:36:16 +0800

    update index and js
    Shifted js to js folder and merged bootstrap and propper

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,22 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+
+            var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+            var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+                return new bootstrap.Popover(popoverTriggerEl)
+            })
+
+            var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+                container: 'body'
+            })
+
+            var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+                trigger: 'focus'
+            })
